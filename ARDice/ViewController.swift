00c9f89e637b1804ec8f817659ecdd7bfc60f83c
Prateek Mahawar <prateekmahawar@gmail.com>
2017-11-05 21:04:26 +0530

    Added Cube

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -20,26 +20,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let node = SCNNode()
+        node.position = SCNVector3Make(0, 0.1, -0.5) //negative z because away from user
+        node.geometry = cube
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+//        // Show statistics such as fps and timing information
+//        sceneView.showsStatistics = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+//        if ARWorldTrackingConfiguration.isSupported {
+//        } else {
+//            let configuration = AROrientationTrackingConfiguration()
+//            sceneView.session.run(configuration)
+//
+//        }
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
+        
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
